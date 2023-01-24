fb078cc3554e1017fd6cd4b0971a939beb37697d
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2023-01-24 12:07:39 +0400

    show cart list & delete from list

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import {Table,Button} from 'reactstrap'
+import React, { Component } from "react";
+import { Table, Button } from "reactstrap";
 
 export default class ProductList extends Component {
   render() {
@@ -7,32 +7,37 @@ export default class ProductList extends Component {
       <div>
         <h3>Product/{this.props.currentcategory}</h3>
         <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Product Name</th>
-            <th>Quantity Unit</th>
-            <th>Unit Price</th>
-            <th>Unit in Stock</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-            {
-            this.props.products.map(product=>(
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Quantity Unit</th>
+              <th>Unit Price</th>
+              <th>Unit in Stock</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.products.map((product) => (
               <tr key={product.id}>
-              <th scope="row">{product.id}</th>
-              <td>{product.productName}</td>
-              <td>{product.quantityPerUnit}</td>
-              <td>{product.unitPrice}</td>
-              <td>{product.unitsInStock}</td>
-              <td><Button onClick={()=>this.props.addToCart(product)} color="info">Add</Button></td>
+                <th scope="row">{product.id}</th>
+                <td>{product.productName}</td>
+                <td>{product.quantityPerUnit}</td>
+                <td>{product.unitPrice}</td>
+                <td>{product.unitsInStock}</td>
+                <td>
+                  <Button
+                    onClick={() => this.props.addToCart(product)}
+                    color="info"
+                  >
+                    Add
+                  </Button>
+                </td>
               </tr>
-            ))
-          }
-        </tbody>
-      </Table>
+            ))}
+          </tbody>
+        </Table>
       </div>
-    )
+    );
   }
 }
